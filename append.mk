57cea9414ba774d2f6c38e683bfd610b202ee23c
Hamster Tian <haotia@gmail.com>
2016-11-20 13:25:38 +0800

    build: Avoid conflict with CFLAGS environment variable

--- append.mk
+++ append.mk
@@ -9,16 +9,17 @@ $(LOCAL_MODULE)_LDFLAGS := $(LOCAL_LDFLAGS)
 $(LOCAL_MODULE)_PRIV_OBJS := $(addprefix $(LOCAL_PATH),$(LOCAL_SRC_FILES:.c=.o))
 $(LOCAL_MODULE)_PRIV_DEPS := $(addprefix $(LOCAL_PATH),$(LOCAL_SRC_FILES:.c=.d))
 
-# Use := here!
-# And $($(xxx)_suffix), not $(xxx)_suffix
 .PHONY: $(LOCAL_MODULE)
-$($(LOCAL_MODULE)_PRIV_OBJS) : CFLAGS := $($(LOCAL_MODULE)_PRIV_C_FLAGS)
 $(LOCAL_MODULE) : $($(LOCAL_MODULE)_PRIV_OBJS)
 
+$($(LOCAL_MODULE)_PRIV_OBJS) : LCFLAGS := $($(LOCAL_MODULE)_PRIV_C_FLAGS)
+$($(LOCAL_MODULE)_PRIV_OBJS) : ${@:.o=.c}
+	@$(CC) $(LCFLAGS) ${@:.o=.c} -c -o $@
+
 # sed: change everything before ":" to "${@:.d=.o} $@"
-$($(LOCAL_MODULE)_PRIV_DEPS) : CFLAGS := $($(LOCAL_MODULE)_PRIV_C_FLAGS)
+$($(LOCAL_MODULE)_PRIV_DEPS) : LCFLAGS := $($(LOCAL_MODULE)_PRIV_C_FLAGS)
 $($(LOCAL_MODULE)_PRIV_DEPS) : ${@:.d=.c}
-	@$(CC) -MM ${@:.d=.c} $(CFLAGS) > $@
+	@$(CC) -MM ${@:.d=.c} $(LCFLAGS) > $@
 	@sed -i 's,^.*:,${@:.d=.o} $@ : ,g' $@
 
 # %.o %.d : *.c
