08d05256353ec62b189035c52802a36b7719956a
Hamster Tian <haotia@gmail.com>
2018-05-22 23:02:46 +0800

    build: make deps generation compatibile with macOS
    * `sed` on macOS requires explicit `extension` for `-i`,
      while this argument can be left out in Linux implementation.
      This caused the pattern being recognized as backup extension
      in macOS, and the command line gets messed up.
    
    * Add `-e` to get rid of this ambiguity. Both implementation
      recognize this change.
    
      This fixes "sed: invalid command code x" errors.

--- append.mk
+++ append.mk
@@ -25,7 +25,7 @@ $($(LOCAL_MODULE)_PRIV_OBJS) : ${@:.o=.c}
 $($(LOCAL_MODULE)_PRIV_DEPS) : LCFLAGS := $($(LOCAL_MODULE)_PRIV_C_FLAGS)
 $($(LOCAL_MODULE)_PRIV_DEPS) : ${@:.d=.c}
 	@$(CC) -MM ${@:.d=.c} $(LCFLAGS) > $@
-	@sed -i 's,^.*:,${@:.d=.o} $@ : ,g' $@
+	@sed -i -e 's|^.*:|${@:.d=.o} $@ : |g' $@
 
 # %.o %.d : *.c
 # This will cause regeneration of .d files. Do not do this when cleaning.
